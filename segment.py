8c648293559017b1932f3ca43e5c8c03bafe08dd
Danifernandez1262 <daniel.fernandez.fernandez@estudiantat.upc.edu>
2025-04-22 19:00:15 +0200

    Delete segment.py

--- segment.py
+++
@@ -1,8 +0,0 @@
-from node import *
-
-class Segment:
-    def __init__(self, name: str, origin: Node, destination: Node):
-        self.name = name
-        self.origin = origin
-        self.destination = destination
-        self.cost = distance(origin, destination)
